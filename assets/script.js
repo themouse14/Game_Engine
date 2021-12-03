96919b9c5481491725ad8822b81e3ca47648f316
David Gavino <ggavino2@uccs.edu>
2021-12-02 21:00:48 -0700

    added table html

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 async function testCall() {
     await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
-.then(response => response.json())
-.then(games => console.log(games))
+        .then(response => response.json())
+        .then(games => console.log(games))
 
 }
 
@@ -12,14 +12,11 @@ testCall();
 // Second API Call
 
 async function testCall2() {
-	await fetch("https://www.cheapshark.com/api/1.0/stores")
-.then(response => response.json())
-.then(gamesstore => console.log(gamesstore))
+    await fetch("https://www.cheapshark.com/api/1.0/stores")
+        .then(response => response.json())
+        .then(gamesstore => console.log(gamesstore))
 
 }
 
 testCall2();
-
-
-
 
