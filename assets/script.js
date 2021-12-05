064b61696b3852e8337d8558bcf3e7e72ff569e3
David Gavino <ggavino2@uccs.edu>
2021-12-05 12:36:33 -0700

    nochange

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 
 async function testCall() {
     await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
-.then(response => response.json())
-.then(games => console.log(games))
+        .then(response => response.json())
+        .then(games => console.log(games))
 
 }
 
@@ -12,9 +12,9 @@ testCall();
 // Second API Call
 
 async function testCall2() {
-	await fetch("https://www.cheapshark.com/api/1.0/stores")
-.then(response => response.json())
-.then(gamesstore => console.log(gamesstore))
+    await fetch("https://www.cheapshark.com/api/1.0/stores")
+        .then(response => response.json())
+        .then(gamesstore => console.log(gamesstore))
 
 }
 
@@ -22,13 +22,4 @@ testCall2();
 
 
 // Functions to pull data from APIs
-
-// Genre Function
-
-
-
-
-
-
-
 
