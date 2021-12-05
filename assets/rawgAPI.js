f11fa31834d98d50427864a0a6361b7d2cb771b7
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-05 15:24:43 -0700

    messing around with code

--- assets/rawgAPI.js
+++ assets/rawgAPI.js
@@ -1,8 +1,9 @@
 // First API Call
 
-async function testCall() {
-    await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
-        .then(response => response.json())
-        .then(games => console.log(games))
-}
-testCall();
+// async function testCall() {
+//     await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
+//         .then(response => response.json())
+//         .then(games => console.log(games))
+// }
+// testCall();
+
