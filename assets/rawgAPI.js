eac0113278a49cf7dbbe176e94323c970a8aac9d
RMRBoot <rhettmrichardson@gmail.com>
2021-12-05 15:51:59 -0700

    selector logic

--- assets/rawgAPI.js
+++ assets/rawgAPI.js
@@ -1,8 +1,17 @@
 // First API Call
 
-async function testCall() {
-    await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
+async function getGameData(genres) {
+    await fetch(`https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167&genres=${genres}`)
         .then(response => response.json())
         .then(games => console.log(games))
+        // const data = response.json();
 }
-testCall();
+getGameData("action");
+
+const genre = document.querySelector("#genres1");
+console.log("genre",genre);
+
+genre.addEventListener("click", function() {
+    console.log("button was clicked");
+    
+});
