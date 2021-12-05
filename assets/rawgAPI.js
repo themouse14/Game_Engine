e82170bc38b2f6bbab0f94a30ab178e9389575f9
RMRBoot <rhettmrichardson@gmail.com>
2021-12-05 16:29:15 -0700

    javascript dropdown target

--- assets/rawgAPI.js
+++ assets/rawgAPI.js
@@ -1,4 +1,9 @@
 // First API Call
+const genre = document.querySelector("#genres1");
+const genredropdown = document.querySelector("#genredrop");
+const dropdown1 = document.querySelector("#dropdownMenu2");
+
+
 
 async function getGameData(genres) {
     await fetch(`https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167&genres=${genres}`)
@@ -8,10 +13,12 @@ async function getGameData(genres) {
 }
 getGameData("action");
 
-const genre = document.querySelector("#genres1");
+
 console.log("genre",genre);
 
 genre.addEventListener("click", function() {
     console.log("button was clicked");
-    
+    // add class "show" to genredropdown
+    // add class "show" to dropdown1
+    // if "show" class exists, remove else add
 });
