e245d70ec59c6e26f1fba73b1275f44a861b6231
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-06 15:07:30 -0700

    experimenting with other api's

--- assets/cheapSharkAPI.js
+++ assets/cheapSharkAPI.js
@@ -1,11 +1,11 @@
 // Second API Call
 
-async function testCall2() {
-    await fetch("https://www.cheapshark.com/api/1.0/stores")
-        .then(response => response.json())
-        .then(gamesstore => console.log(gamesstore))
-}
-testCall2();
+// async function testCall2() {
+//     await fetch("https://www.cheapshark.com/api/1.0/stores")
+//         .then(response => response.json())
+//         .then(gamesstore => console.log(gamesstore))
+// }
+// testCall2();
 
 
 
--- assets/rawgAPI.js
+++ assets/rawgAPI.js
@@ -1,9 +1,31 @@
-// First API Call
+
 
 // async function testCall() {
 //     await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
 //         .then(response => response.json())
 //         .then(games => console.log(games))
-// }
+// };
 // testCall();
 
+// async function pokemonNames(){
+//     fetch("https://pokedex2.p.rapidapi.com/pokedex/uk", {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-host": "pokedex2.p.rapidapi.com",
+//             "x-rapidapi-key": "b97fd5a2fcmsh67eddc4b5646d20p1d35b3jsnf7b62b25c90a"
+//         }
+//     })
+// .then(response => response.json())
+// .then(pokemon1 => console.log(pokemon1))
+// };
+
+// pokemonNames();
+
+// async function pokemonPrices(){
+//     fetch("https://api.pokemontcg.io/v2/cards?q=name:gardevoir", {
+//     "method": "GET"})
+//     .then(response => response.json())
+//     .then(prices => console.log(prices))
+// };
+
+// pokemonPrices();
