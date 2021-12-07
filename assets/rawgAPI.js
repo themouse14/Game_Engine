c6651a11cbfc3ad5bac62647cc795a44191348a8
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2021-12-06 22:42:36 -0700

    removed rawg & cheap shark api

--- assets/rawgAPI.js
+++ assets/rawgAPI.js
@@ -1,12 +1,3 @@
-
-
-// async function testCall() {
-//     await fetch("https://api.rawg.io/api/games?key=8d248e60c35d4a06aa9e584a6a9d9167")
-//         .then(response => response.json())
-//         .then(games => console.log(games))
-// };
-// testCall();
-
 // async function pokemonNames(){
 //     fetch("https://pokedex2.p.rapidapi.com/pokedex/uk", {
 //         "method": "GET",
